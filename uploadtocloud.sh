db8d0aa4ec1fbc35578254a668a635307fda92bb
AaronTarga <fanat.barca15@gmail.com>
2019-10-05 17:13:03 +0200

    pattern fix
    last dot gets now replaced not first so that filenames with dot inside can get downloaded

--- uploadtocloud.sh
+++ uploadtocloud.sh
@@ -2,7 +2,6 @@
 
 youtube-dl -o '%(title)s.%(ext)s' --extract-audio --audio-format mp3 $1
 file=`youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" --get-filename  $1 --metadata-from-title "%(artist)s - %(title)s" --audio-quality 9`
-file="${file/.*/.mp3}"
 filepath=$(echo "$file" | sed 's/ /\\ /g')
 filepath=$(echo "$filepath" | sed 's/)/\\)/g')
 filepath=$(echo "$filepath" | sed 's/(/\\(/g')
@@ -11,6 +10,7 @@ filepath=$(echo "$filepath" | sed 's/\[/\\\[/g')
 filepath=$(echo "$filepath" | sed 's/\&/\\&/g')
 filepath=$(echo "$filepath" | sed 's/|/\\|/g')
 filepath=$(echo "$filepath" | sed "s/'/\\\'/g")
+filepath=$(echo "$filepath" | sed 's/\.\([^.]*\)$/\.mp3/')
 echo $filepath
 eval "curl -g -u user:password -T $filepath https://nextcloud.example.com/remote.php/dav/files/$user/path/where/you/want/to/save"
 eval "rm $PWD/$filepath"
